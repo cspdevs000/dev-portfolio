d8926d429cfece1f08e66d3f87b0c91ae346bfaf
cspdevs000 <cspdevs000@gmail.com>
2022-01-19 17:42:52 -0500

    added contact page to home

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,15 +5,52 @@ import './Home.css';
 
 
 class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: "",
+            email: "",
+            message: ""
+        };
+    }
+
+    handleName(e) {
+        this.setState({
+            name: e.target.value,
+        });
+    }
+
+    handleEmail(e) {
+        this.setState({
+            email: e.target.value,
+        });
+    }
+
+    handleMessage(e) {
+        this.setState({
+            message: e.target.value,
+        });
+    }
+    
     render() {
         return (
             <div className='home-container'>
                 <Animation />
                 <div className='bkgd-container'>
-                <img src={bkgdSpin} className="App-logo" alt="logo" />
+                    <img src={bkgdSpin} className="App-logo" alt="logo" />
                 </div>
                 <div className='about-me-countainer'>
-                <a className="about-me-link" href="/about" >About Me </a>
+                    <a className="about-me-link" href="/about" >About Me </a>
+                </div>
+                <div className='contact-form-container'>
+                    <h1>Contact Me</h1>
+                    {/* <input type="text" id="show-button">Contact Me</input> */}
+                    <form className='contact-form' action="https://formsubmit.co/cspdevs000@gmail.com" method="POST">
+                        <input className="contact-field" placeholder= "name"onChange={this.handleName.bind(this)} type="text" name="name" required /><br />
+                        <input className="contact-field" id="contact-email" placeholder= "email" onChange={this.handleEmail.bind(this)} type="email" name="email" required /><br />
+                        <input className="contact-form-textbox" placeholder="message" onChange={this.handleMessage.bind(this)} type="text" name="message" required /><br />
+                        <button className="contact-form-button" type="submit">Send it</button>
+                    </form>
                 </div>
             </div>
         );
