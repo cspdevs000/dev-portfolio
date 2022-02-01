66cdc88d9c77b9002d3eb5bb1e116b82c7769e8f
cspdevs000 <cspdevs000@gmail.com>
2022-01-31 19:54:36 -0500

    added neighborly link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,6 +8,7 @@ class Navbar extends Component {
             <div className='navbar-container'>
                 <a className="navbar-link" href="https://cspdevs000.github.io/G5-tower-ascender/" target="_blank">G5 Tower Ascender</a>
                 <a className="navbar-link" href="https://my-parks.herokuapp.com/" target="_blank">myParks</a>
+                <a className="navbar-link" href="https://neighborly-application.herokuapp.com/" target="_blank">Neighborly</a>
                 <a className="navbar-link" href="https://viral-covid.herokuapp.com/" target="_blank">Viral</a>
             </div>
         );
