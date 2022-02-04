c4d8dac866ce0b7464d37d311d0fc36a0b7a6e1d
cspdevs000 <cspdevs000@gmail.com>
2022-02-04 03:40:27 -0500

    updated links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ class Navbar extends Component {
     render() {
         return (
             <div className='navbar-container'>
-                <a className="navbar-link" href="https://cspdevs000.github.io/G5-tower-ascender/" target="_blank">G5 Tower Ascender</a>
+                {/* <a className="navbar-link" href="https://cspdevs000.github.io/G5-tower-ascender/" target="_blank">G5 Tower Ascender</a> */}
                 <a className="navbar-link" href="https://my-parks.herokuapp.com/" target="_blank">myParks</a>
                 <a className="navbar-link" href="https://neighborly-application.herokuapp.com/" target="_blank">Neighborly</a>
                 <a className="navbar-link" href="https://viral-covid.herokuapp.com/" target="_blank">Viral</a>
