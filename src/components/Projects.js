5125f3847e4e3c7da098919fe208fbcd85707eae
cspdevs000 <cspdevs000@gmail.com>
2022-02-08 15:35:00 -0500

    more detailed prject descriptions

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -12,8 +12,8 @@ class Projects extends Component {
             <div className='projects-container'>
                 <div className='project-card'>
                     <h2 className='project-title'>Neighborly</h2>
-                    <a href='https://neighborly-application.herokuapp.com/' target="_blank" className='project-img-a'>
-                        <img src={neighborly} className='project-img'/>
+                    <a href='https://neighborly-application.herokuapp.com/' target="_blank" rel="noreferrer" className='project-img-a'>
+                        <img src={neighborly} alt="project-1" className='project-img'/>
                     </a>
                     <p className='project-description'>
                         A closed social media application designed entirely in Django. If you'd like to test the search, search for the word 'test'. If you'd like to test user functionality with an already populated message board, sign in with username: testuser1, password: berenstein
@@ -21,29 +21,29 @@ class Projects extends Component {
                 </div>
                 <div className='project-card'>
                     <h2 className='project-title'>Viral</h2>
-                    <a href='https://viral-covid.herokuapp.com/' target="_blank" className='project-img-a'>
-                        <img src={viral} className='project-img'/>
+                    <a href='https://viral-covid.herokuapp.com/' target="_blank" rel="noreferrer" className='project-img-a'>
+                        <img src={viral} alt="project-2" className='project-img'/>
                     </a>
                     <p className='project-description'>
-                        A MERN application for getting COVID data by county. User can update vaccine card photo and set the county they live in for default information.
+                        A MERN application for getting COVID data by county. User can update vaccine card photo and set the county they live in for easy access to relevant information. Please forgive Heroku for the slow initial loading time of the landing page.
                     </p>
                 </div>
                 <div className='project-card'>
                     <h2 className='project-title'>myParks</h2>
-                    <a href='https://my-parks.herokuapp.com/' target="_blank" className='project-img-a'>
-                        <img src={myparks} className='project-img'/>
+                    <a href='https://my-parks.herokuapp.com/' target="_blank" rel="noreferrer" className='project-img-a'>
+                        <img src={myparks} alt="project-3" className='project-img'/>
                     </a>
                     <p className='project-description'>
-                        An EJS / express application for archiving user's national park visits and memories within.
+                        An EJS / express application for archiving user's national park visits. User can keep a list of parks they've visited, view park info and add 'memory' posts.
                     </p>
                 </div>
                 <div className='project-card'>
                     <h2 className='project-title'>G5 Tower Ascender</h2>
-                    <a href='https://cspdevs000.github.io/G5-tower-ascender/' target="_blank" className='project-img-a'>
-                        <img src={g5towerascender} className='project-img'/>
+                    <a href='https://cspdevs000.github.io/G5-tower-ascender/' rel="noreferrer" target="_blank" className='project-img-a'>
+                        <img src={g5towerascender} alt="project-4" className='project-img'/>
                     </a>
                     <p className='project-description'>
-                        My first development project, a simple HTML browser game. Use a & d keys to move the character to either side of the tower.
+                        My first development project, a cheeky and simple HTML browser game. Use a & d keys to move the character to either side of the tower.
                     </p>
                 </div>
             </div>
